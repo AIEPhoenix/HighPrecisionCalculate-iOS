65687978856c1b24a10ec9b8a8bf31359f1838f4
AIEPhoenix <brianlee93@vip.qq.com>
2017-02-27 13:46:52 +0800

    testing

--- HighPrecisionCalculate/HighPrecisionCalculate/HighPrecisionCalculateTest.playground/Contents.swift
+++ HighPrecisionCalculate/HighPrecisionCalculate/HighPrecisionCalculateTest.playground/Contents.swift
@@ -11,8 +11,41 @@ var z = "a".decimalNumberValue() ?? "1".decimalNumberValue()
 ///Calculate
 //Addition
 x + 1
+1 + x
 x + 1.1
+1.1 + x
 x++
 ++x
 x += 1
 x.stringValue
+//Subtraction
+x - 1
+1 - x
+x - 1.1
+1.1 - x
+--x
+x--
+x -= 1
+x.stringValue
+//Multiplication
+x * 2
+2 * x
+x * 1.5
+1.5 * x
+x *= 1.5
+//Division
+x / 2
+2 / x
+x / 1.5
+1.5 / x
+x /= 1.5
+//Compare
+1.decimalNumberValue() > 0
+1.decimalNumberValue() < 0
+
+1.decimalNumberValue() > 1.1
+1.decimalNumberValue() < 1.1
+
+1.decimalNumberValue() >= 1
+1.decimalNumberValue() >= 0.5 //false
+1.decimalNumberValue() >= 1.1 //false
